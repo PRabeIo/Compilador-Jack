ba566b3c2c4d63486ae5ace0cfcdfcaeb759b203
PRabeIo <duduzera_10@hotmail.com>
2026-04-07 18:55:24 -0300

    Implementado TokenType

---
+++ README.md
@@ -0,0 +1,15 @@
+# Projeto Compilador Jack
+
+Este repositório é dedicado ao desenvolvimento de um compilador para a linguagem *Jack*, conforme proposto na disciplina de Compiladores///////.
+
+*Curso:* Engenharia da Computação - CCET - UFMA
+*Professor:* Sergio Souza Costa
+
+## 👥 Equipe
+* *Paulo Eduardo Lima Rabelo*
+  * Matrícula: 20260001203
+* *Italo Francisco Almeida de Oliveira*
+  * Matrícula: 20260001230 
+
+## 🛠️ Tecnologias Utilizadas
+* *Linguagem de Programação:* Java
---
+++ src/TokenType.java
@@ -0,0 +1,59 @@
+public enum TokenType {
+	
+    CLASS("keyword"),
+    CONSTRUCTOR("keyword"),
+    FUNCTION("keyword"),
+    METHOD("keyword"),
+    FIELD("keyword"),
+    STATIC("keyword"),
+    VAR("keyword"),
+    INT("keyword"),
+    CHAR("keyword"),
+    BOOLEAN("keyword"),
+    VOID("keyword"),
+    TRUE("keyword"),
+    FALSE("keyword"),
+    NULL("keyword"),
+    THIS("keyword"),
+    LET("keyword"),
+    DO("keyword"),
+    IF("keyword"),
+    ELSE("keyword"),
+    WHILE("keyword"),
+    RETURN("keyword"),
+    
+    LPAREN("symbol"),
+    RPAREN("symbol"),
+    LBRACE("symbol"),
+    RBRACE("symbol"),
+    LBRACKET("symbol"),
+    RBRACKET("symbol"),
+    COMMA("symbol"),
+    SEMICOLON("symbol"),
+    DOT("symbol"),
+    PLUS("symbol"),
+    MINUS("symbol"),
+    ASTERISK("symbol"),
+    SLASH("symbol"),
+    AND("symbol"),
+    OR("symbol"),
+    NOT("symbol"),
+    LT("symbol"),
+    GT("symbol"),
+    EQ("symbol"),
+
+    NUMBER("integerConstant"),
+    STRING("stringConstant"),
+    IDENT("identifier"),
+    EOF("eof");
+    
+    final String xmlTag;
+
+    TokenType(String xmlTag) {
+        this.xmlTag = xmlTag;
+    }
+
+    public String getXmlTag() { 
+        return xmlTag;
+    }
+}
