d14a887ffbbe16f487e9d136b18c2fdea4424431
Italorde <italo.fao@discente.ufma.br>
2026-04-07 21:24:16 -0300

    Adicionando o XML Generator

--- src/Token.java
+++ src/Token.java
@@ -1,5 +1,5 @@
 public class Token {
-    public final TokenType tag;
+    public final TokenType tag; 
     public final String value;
 
     public Token(TokenType tag, String value) {
@@ -9,6 +9,17 @@ public class Token {
 
     public TokenType getType()  { return tag; }
     public String    getValue() { return value; }
+
+    public String toXML() {
+        return "<" + tag.getXmlTag() + "> " + escapeXml(value) + " </" + tag.getXmlTag() + ">";
+    }
+
+    private static String escapeXml(String s) {
+        return s.replace("&", "&amp;")
+                .replace("<", "&lt;")
+                .replace(">", "&gt;")
+                .replace("\"", "&quot;");
+    }
 
     @Override
     public String toString() {
